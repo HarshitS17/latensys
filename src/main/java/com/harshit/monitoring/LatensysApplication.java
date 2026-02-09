377c0a6ccc3756bdc63c7b205fdeca04ae156329
Harshit Saini <sainiharshit0017@gmail.com>
2026-02-09 19:56:39 +0530

    Initial commit: Latensys API monitoring platform

--- src/main/java/com/harshit/monitoring/LatensysApplication.java
+++ src/main/java/com/harshit/monitoring/LatensysApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiMonitoringPlatformApplication {
+public class LatensysApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiMonitoringPlatformApplication.class, args);
+        SpringApplication.run(LatensysApplication.class, args);
     }
 
 }
